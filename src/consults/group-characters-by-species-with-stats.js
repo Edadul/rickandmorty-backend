db74b30be24b171fd832ef32280ef65762cc5e67
AndresMonserrat <monserrata@uninorte.edu.co>
2026-09-03 22:19:54 -0500

    feat: Implement grouping of characters by species with stats

--- src/consults/group-characters-by-species-with-stats.js
+++ src/consults/group-characters-by-species-with-stats.js
@@ -1,41 +1,28 @@
-const aliveHumanCharacters = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    type: "Parasite",
-    gender: "Male",
-    origin: "Earth (C-137)",
-    currentLocation: "Citadel of Ricks",
-    episodeOccurence: 51,
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    status: "Alive",
-    species: "Human",
-    type: "alien",
-    gender: "Male",
-    origin: "unknown",
-    currentLocation: "Citadel of Ricks",
-    episodeOccurence: 51,
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-  },
-];
-
 export const charactersBySpeciesWithStats = (characters) => {
   return characters.reduce((acumulador, character) => {
     const type = character.type || "Unknown";
 
+    // Si no existe, inicializar
+    if (!acumulador[type]) {
+      acumulador[type] = {
+        cantidad: 0,
+        totalEpisodios: 0,
+        vivos: 0,
+        promedioEpisodios: 0,
+      };
+    }
 
-    acumulador[type] = { cantidad: (acumulador[type]?.cantidad || 0) + 1, promedioEpisodios: 0, vivos: acumulador };
+    // Ahora actualizamos (no sobrescribimos)
+    acumulador[type].cantidad += 1;
+    acumulador[type].totalEpisodios += character.episodeOccurence;
+    
+    if (character.status === "Alive") {
+      acumulador[type].vivos += 1;
+    }
+
+    acumulador[type].promedioEpisodios = 
+      acumulador[type].totalEpisodios / acumulador[type].cantidad;
 
     return acumulador;
   }, {});
 };
-
-const groupedCharacters = charactersBySpeciesWithStats(aliveHumanCharacters);
-
-console.log(groupedCharacters);
